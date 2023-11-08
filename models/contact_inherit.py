49ca9580b282782ee41d6af344e431a4f7065abd
waelben09 <waelbenkherfallah@gmail.com>
2023-11-08 02:17:54 +0100

    Calcul du Nombre de Voyages

--- models/contact_inherit.py
+++ models/contact_inherit.py
@@ -6,3 +6,5 @@ class ResPartner(models.Model):
     _inherit = "res.partner"
     #champs relationnel pour recuperer tous les voyage de notre contact courant
     voyages = fields.One2many("contact_travel.voyage", "contact", string="Voyages")
+    # compteur de nombre de voyage dans le champs précédent
+    voyages_count = fields.Integer(compute="get_voyages_length")
